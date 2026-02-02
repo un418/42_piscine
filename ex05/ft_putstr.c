3bb849033de6cbd2723801f35e9922423e21f15f
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-02 19:58:11 +0000

    improve - ft_putstr.c from C01/ex05 - normi + test OK

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 18:44:52 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/02 19:46:34 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/02 19:57:12 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,17 @@
 
 void	ft_putstr(char *str)
 {
-	char test_char;
-	test_char = *str;
-	while (test_char != '\0')
+	while (*str != '\0')
 	{
 		write(1, str, 1);
-		test_char = *str;
 		str++;
 	}
 }
-
+/* 
 int	main(void)
 {
-	char str[] = "Lisboa 2026/n";
+	char str[] = "Lisboa 2026\n";
 	ft_putstr(str);
 	return (0);
 }
+ */
