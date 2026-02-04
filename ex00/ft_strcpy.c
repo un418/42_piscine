a6bcdd76fce04b205c2336b9b60786ef32ae433c
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-04 21:11:01 +0000

    add - draft ft_strcpy.c

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,15 +6,45 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 20:50:29 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/03 20:51:58 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/04 21:10:24 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	//man strcpy 
-}
-int main(void)
-{
+	int i;
 
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return dest;
+}
+
+#include <unistd.h>
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(void)
+{
+	char src[30] = "Lisboa 2026\n";
+	char dst[8] = "11111111";
+	char segf[10] = "12";
+
+	ft_strcpy(dst, src);
+	ft_putstr(dst);
+	ft_putstr(segf);
+	
+	return (0);
 }
