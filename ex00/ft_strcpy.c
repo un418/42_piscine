af6d472fecaeeeea17d795ecae1e70d18f667a76
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-06 12:45:31 +0000

    improve ft_strcpy.c - normi + test OK

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,45 +6,51 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 20:50:29 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/04 21:10:24 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/06 12:42:49 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	int i;
+	int	i;
 
 	i = 0;
-
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-
-	return dest;
+	return (dest);
 }
 
+/* 
 #include <unistd.h>
 void	ft_putstr(char *str)
 {
-	while (*str != '\0')
+	// while (*str != '\0')
+	int count = 0;
+	while (count <= 16)
 	{
 		write(1, str, 1);
 		str++;
+		count++;
 	}
 }
 
 int	main(void)
 {
-	char src[30] = "Lisboa 2026\n";
-	char dst[8] = "11111111";
-	char segf[10] = "12";
+	char *src = "Lisboa 2026\n";
+	char dst[20] = "aaaaaaaaaaaaaaaa";
 
 	ft_strcpy(dst, src);
-	ft_putstr(dst);
-	ft_putstr(segf);
+	//ft_putstr(dst);
+	ft_putstr(ft_strcpy(dst, src));
 	
 	return (0);
 }
+
+/* Tested:
+	- Overflow : Able to copy src[20] to dest [5] // 
+	- src[5] to dest[10] - Able to access what in end of dest
+*/
