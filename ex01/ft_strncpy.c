7a42c52e13923e793a600230fc19c158b7683406
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-07 15:19:18 +0000

    improve - ft_strncpy.c from C02/ex01 - reproduce exact logic in man strcpy + more test OK + normi OK

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 13:20:15 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/07 14:35:53 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/07 15:18:12 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (n >= i)
+	while (i < n && src[i] != '\0')
 	{
-		while (i < n)
-		{
-			dest[i] = src[i];
-			i++;
-		}
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
 		dest[i] = '\0';
 		i++;
 	}
@@ -32,13 +32,19 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 #include <stdio.h>
 int	main(void)
 {
-	unsigned int size = 8;
-	char str_src[10]="Test42";
-	char str_dst[10]="aaaaaaaaaa";
+	// unsigned int size = 3;
+	// char str_src[10]="Test42";
+	// char str_dst[10]="aaaaaaaaaa";
 
+	unsigned int size = 3;
+	char str_src[10]="aaaaaaaaaa";
+	char str_dst[10]="Test42";
+
+	// unsigned int size = 0;
 	// char str_src[0]="T";
 	// char str_dst[0];
 
+	// unsigned int size = 0;
 	// char str_src[0]="";
 	// char str_dst[0];
 
