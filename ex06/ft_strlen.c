d5b2db175fc90dc8c24bc7a03f62b86eb43dd056
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-09 15:14:26 +0000

    add test for empty array - C01/ex06

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 13:14:44 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/03 13:23:00 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/09 15:12:42 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,9 @@ int	main(void)
 {
 	int	strlen;
 	
-	char str[] = "Lisboa sahgckctrfxjtrzyhtrxhjnrsyxjxmxf2026\n";
+	// char str[] = "Lisboa sahgckctrfxjtrzyhtrxhjnrsyxjxmxf2026\n";
+	char str[] = "";
+	// char str[] = "12345";
 	strlen = ft_strlen(str);
 	printf("debug: strlen=%d \n", strlen);
 	return (0);
