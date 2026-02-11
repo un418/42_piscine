a7e5c86f16a64cb7084e25bab7acd89c79f18f53
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-11 21:44:46 +0000

    improve ft_str_is_numeric.c - rewrite in negative yoda mode for fun  and redo test before eval - Normi +Test OK - C02/ex03

--- ex03/ft_str_is_numeric.c
+++ ex03/ft_str_is_numeric.c
@@ -6,9 +6,15 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 16:00:57 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/07 16:08:04 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/11 21:40:05 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* 
+Create a function that returns 1 if the given string contains 
+only digits and 0 if it contains any other character.
+It should return 1 if str is empty.
+*/
 
 int	ft_str_is_numeric(char *str)
 {
@@ -17,7 +23,7 @@ int	ft_str_is_numeric(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] < '0' || str[i] > '9')
+		if (!('0' <= str[i] && str[i] <= '9'))
 			return (0);
 		i++;
 	}
@@ -28,12 +34,13 @@ int	ft_str_is_numeric(char *str)
 int	main(void)
 {
 	// char str_main[]="0123456789";
-	char str_main[]="0123456AAA";
-	// char str_main[]="0123456$-_AAA";
+	// char str_main[]="0123456AAA";
+	char str_main[]="0123456$-_AAA";
     // char str_main[]="";
 	printf("return:%d",ft_str_is_numeric(str_main));
 }
 
+// Before Yoda
 // If str[i] is outstide a to z
 // if (str[i] < '0' || str[i] > '9')
  */
