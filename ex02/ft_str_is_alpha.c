8c3c2356b06d1dedc7b31568678d846e433b2af8
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-11 20:48:21 +0000

    improve ft_str_is_alpha.c - rewrite in negative yoda mode for fun  and redo test before eval - Normi +Test OK - C02/ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,9 +6,16 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 15:28:46 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/07 16:15:31 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/11 21:43:17 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* 
+Create a function that returns 1 if the given string 
+contains only alphabetical characters
+and 0 if it contains any other character.
+It should return 1 if str is empty.
+*/
 
 int	ft_str_is_alpha(char *str)
 {
@@ -17,7 +24,8 @@ int	ft_str_is_alpha(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] < 'a' || str[i] > 'z') || (str[i] < 'A' || str[i] < 'Z'))
+		if (!(('a' <= str[i] && str[i] <= 'z')
+				|| ('A' <= str[i] && str[i] <= 'Z')))
 			return (0);
 		i++;
 	}
@@ -34,6 +42,7 @@ int	main(void)
 	printf("return:%d",ft_str_is_alpha(str_main));
 }
 
+// First Condition
 // If str[i] is (outstide a to z) OR (outside A to Z)
 // if ((str[i] < 'a' || str[i] > 'z') || (str[i] < 'A' || str[i] < 'Z'))
  */
