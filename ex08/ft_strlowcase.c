cdba341070661501ad1de65d18c209404165b0f8
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-12 15:22:12 +0000

    improve ft_strlowcase.c - rewrite in negative yoda mode for fun  and redo test before eval - Normi +Test OK - C02/ex08

--- ex08/ft_strlowcase.c
+++ ex08/ft_strlowcase.c
@@ -6,9 +6,14 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 18:36:29 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/07 18:37:52 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/12 15:23:00 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* 
+Create a function that converts every letter to lowercase.
+It should return str
+*/
 
 char	*ft_strlowcase(char *str)
 {
@@ -17,20 +22,26 @@ char	*ft_strlowcase(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
+		if ('A' <= str[i] && str[i] <= 'Z')
 			str[i] = str[i] + 32;
 		i++;
 	}
 	return (str);
 }
+
 /* 
 #include <stdio.h>
 int	main(void)
 {
-	// char str_main[]="aIGUfojpdbUGI@#$%^&*(ohpguoryojpuogy8hopo";
-	char str_main[]="abchdRTYH";
+	// char str_main[]="aIGUfojpdbUGI@#$%^&*(ohpguorQWERTyojpuogy8hopo";
+	// char str_main[]="abchdRTYH";
+	char str_main[]="";
 	printf("input:%s",str_main);
 	printf("\ndebug:%s",ft_strlowcase(str_main));
 	return (0);
 }
  */
+// Before Yoda
+// if (str[i] >= 'A' && str[i] <= 'Z')
+// if item is superior = to A AND inferior = to 'Z'
+// Means between 'A' AND 'Z'
