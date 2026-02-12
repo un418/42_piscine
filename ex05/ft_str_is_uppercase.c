8eb63d6e7ba8d76302c072b18e0bbf5b909491d2
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-12 14:53:41 +0000

    improve ft_str_is_uppercase.c - rewrite in negative yoda mode for fun  and redo test before eval - Normi +Test OK - C02/ex05

--- ex05/ft_str_is_uppercase.c
+++ ex05/ft_str_is_uppercase.c
@@ -6,9 +6,15 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 16:45:22 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/07 16:47:20 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/12 14:53:11 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* 
+Create a function that returns 1 if the given string contains only uppercase
+alphabetical characters and 0 if it contains any other character.
+It should return 1 if str is empty.
+*/
 
 int	ft_str_is_uppercase(char *str)
 {
@@ -17,20 +23,25 @@ int	ft_str_is_uppercase(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] < 'A' || str[i] > 'Z')
+		if (!('A' <= str[i] && str[i] <= 'Z'))
 			return (0);
 		i++;
 	}
 	return (1);
 }
-/* 
+
+
 #include <stdio.h>
 int	main(void)
 {
-	// char str_main[]="AAAAAAAAAA";
+	char str_main[]="AAAAAAAAAA";
 	// char str_main[]="AAAAAaaAAAA";
 	// char str_main[]="aaaaaa-5aaaaa";
-	char str_main[]="";
+	// char str_main[]="";
 	printf("return:%d",ft_str_is_uppercase(str_main));
 }
- */
+
+// Before Yoda
+// if (str[i] < 'A' || str[i] > 'Z')
+// if item is inferior to 'A' OR superior to 'Z'
+// means not between A and Z
