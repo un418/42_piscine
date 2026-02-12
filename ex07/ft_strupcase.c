fc0f27e96b089319dae69f14007d8a4575aff7e8
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-12 15:17:45 +0000

    improve ft_strupcase.c - rewrite in negative yoda mode for fun  and redo test before eval - Normi +Test OK - C02/ex07

--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -6,10 +6,14 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 18:06:03 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/07 18:34:30 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/12 15:17:19 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* 
+Create a function that converts every letter to uppercase
+It should return str.
+*/
 char	*ft_strupcase(char *str)
 {
 	int	i;
@@ -17,7 +21,7 @@ char	*ft_strupcase(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if ('a' <= str[i] && str[i] <= 'z')
 			str[i] = str[i] - 32;
 		i++;
 	}
@@ -27,10 +31,15 @@ char	*ft_strupcase(char *str)
 #include <stdio.h>
 int	main(void)
 {
-	char str_main[]="aIGUfojpdbUGI@#$%^&*(ohpguoryojpuogy8hopo";
+	// char str_main[]="aIGUfojpdASDERbUGI@#$%^&*(ohpguoryojpuogy8hopo";
 	// char str_main[]="abchdRTYH";
+	char str_main[]="";
 	printf("input:%s",str_main);
 	printf("\ndebug:%s",ft_strupcase(str_main));
 	return (0);
 }
  */
+// Before Yoda
+// if (str[i] >= 'a' && str[i] <= 'z')
+// if item is superior = to 'a' AND inferior = to 'z'
+// Means between 'a' ans 'z'
