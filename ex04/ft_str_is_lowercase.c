ebaaaffbe6bf6cede269510d89c777e302b1498b
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-12 14:44:19 +0000

    improve ft_str_is_lowercase.c - rewrite in negative yoda mode for fun  and redo test before eval - Normi +Test OK - C02/ex04

--- ex04/ft_str_is_lowercase.c
+++ ex04/ft_str_is_lowercase.c
@@ -6,9 +6,15 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 16:37:44 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/07 16:43:01 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/12 14:42:34 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* 
+Create a function that returns 1 if the given string contains only lowercase
+alphabetical characters and 0 if it contains any other character.
+It should return 1 if str is empty.
+*/
 
 int	ft_str_is_lowercase(char *str)
 {
@@ -17,7 +23,7 @@ int	ft_str_is_lowercase(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] < 'a' || str[i] > 'z')
+		if (!('a' <= str[i] && str[i] <= 'z'))
 			return (0);
 		i++;
 	}
@@ -29,8 +35,13 @@ int	main(void)
 {
 	// char str_main[]="aaaaaaaaaaa";
 	// char str_main[]="aaaaaaAaaaaa";
-	// char str_main[]="aaaaaa-5aaaaa";
-	char str_main[]="";
+	char str_main[]="aaaaaa-5aaaaa";
+	// char str_main[]="";
 	printf("return:%d",ft_str_is_lowercase(str_main));
 }
  */
+
+// Before Yoda
+// if (str[i] < 'a' || str[i] > 'z')
+// if item is inferior to 'a' or superior to 'z'
+// means no between 'a' and 'z'
