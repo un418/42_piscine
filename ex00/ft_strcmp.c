35141d379a9e07c7c94dfac9c6343ed286a82123
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-16 14:10:51 +0000

    Recode totally ft_strcmp from advice from my peers - C03/ex00

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 17:56:10 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/12 16:15:44 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/16 14:07:26 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,57 +18,58 @@ strcmp() returns an integer indicating the result of the comparison, as follows
        • a positive value if s1 is greater than s2.
 The strncmp() function is similar,
 except it compares only the first (at most) n bytes of s1 and s2.
+
+MORE DETAILLED INFO
+- https://www.w3schools.com/c/ref_string_strcmp.php
 */
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
-	int	diff;
 
 	i = 0;
-	diff = 0;
-	while (s1[i])
+	while (s1[i] || s2[i])
 	{
-		diff = diff + s1[i];
-		i++;
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
 	}
-	i = 0;
-	while (s2[i])
-	{
-		diff = diff - s2[i];
-		i++;
-	}
-	return (diff);
+	return (0);
 }
+
 /* 
 #include <stdio.h>
 int main(void)
 {
-	// char str1[]="Hello";
-	// char str2[]="Hello";
-	// 0
-
-	// char str1[]="Hello0";
-	// char str2[]="Hello";
-	// 48
-
-	// char str1[]="Hello";
-	// char str2[]="Hello0";
-	// -48
-
-	// char str1[]="";
-	// char str2[]="";
-	// 0
-
-	// char str1[]="H";
-	// char str2[]="";
-	// 72
-
-	char str1[]="";
-	char str2[]="~";
-	// -126
-
-	printf("debug:%d",ft_strcmp(str1,str2));
+	
+	char str1[]="Hello";
+	char str2[]="Hello0";
+	char str3[]="";
+	
+	
+	printf("debug_equal:%d\n",ft_strcmp(str1,str1));
+	printf("debug_s1<s2:%d\n",ft_strcmp(str1,str2));
+	printf("debug_s1>s2:%d\n",ft_strcmp(str1,str3));
+	printf("debug_null:%d\n",ft_strcmp(str3,str3));
+	printf("debug_null_rev:%d\n",ft_strcmp(str2,str3));
+	
 	return 0;
+}
+ */
+
+/* 
+// Other possible implementation but more complex for my brain
+// If not (...&& s1[i] != '\0') -> SegFault
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
  */
