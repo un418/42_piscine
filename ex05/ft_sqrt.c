d8ade0ec26ad4a6e3c98f1553702f02272ea420c
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-17 18:59:51 +0000

    add draft - ft_sqrt.c - C05/ex05

---
+++ ex05/ft_sqrt.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 18:34:27 by adaferna          #+#    #+#             */
+/*   Updated: 2026/02/17 18:56:38 by adaferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+Create a function that returns the square root of a given number (if it exists), or 0
+if the square root is an irrational number.
+
+irrational number:
+- infinite floating number like PI or 1/3=0.33333333.....
+- The square root of a negative number is not a real number
+
+Square roots:
+- A square root of a number is a value that,
+when multiplied by itself ( x*x or x^2), equals the original number
+
+n = x * x
+*/
+
+int ft_sqrt(int nb)
+{
+
+}
+
+int main(void)
+{
+	printf("sqrt of 9 = %d",ft_sqrt(9));
+}
