f7ed02f204ec66fefab4800a20d84312f460b906
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-17 22:13:12 +0000

    fix - manage edge case and add test - C05/ex06

--- ex06/ft_is_prime.c
+++ ex06/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 19:31:02 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/17 19:57:51 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/17 22:11:23 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,16 @@ Prime numbers are natural numbers that are divisible by
 In other words, prime numbers are positive integers greater than 1 
 	with exactly two factors, 1 and the number itself.
 
-So if we can modulo 0 from 2 to n-1 means its a prime number.
+So if we can modulo 0 from 2 to n-1 means its a not prime number.
 */
+
 int	ft_is_prime(int nb)
 {
 	int	i;
 
 	i = 2;
+	if ( nb <= 0)
+		return (0);
 	while (i < nb)
 	{
 		if (nb % i == 0)
@@ -39,12 +42,17 @@ int	ft_is_prime(int nb)
 #include <stdio.h>
 int main()
 {
-	int i = 0;
+	int i = -5;
 	while (i < 100)
 	{
 		if (ft_is_prime(i)) // Avoid null result
 			printf("%d is a prime number\n", i);
 		i++;
 	}
+
+	printf("0 is a prime?  %d \n", ft_is_prime(0));
+	printf("1 is a prime?  %d \n", ft_is_prime(1));
+	printf("-5 is a prime?  %d \n", ft_is_prime(-5));
+	printf("-12 is a prime?  %d \n", ft_is_prime(-12));
 }
  */
