fecb5ca0e353ab1a6018a56914ab7e632b702681
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-18 00:58:35 +0000

    add ft_print_program_name.c - C06/ex00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 00:52:04 by adaferna          #+#    #+#             */
+/*   Updated: 2026/02/18 00:53:50 by adaferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Write a program that displays its own name, followed by a new line.
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	ft_putstr(argv[0]);
+	write(1, "\n", 1);
+}
