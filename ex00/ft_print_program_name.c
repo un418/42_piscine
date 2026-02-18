6a17b11affc6bf33cf9d695ef8f18571df0d09d1
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-18 00:12:40 +0000

    add ft_print_program_name.c - C06/ex01

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 23:11:51 by adaferna          #+#    #+#             */
+/*   Updated: 2026/02/18 00:10:10 by adaferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+Since this is a program, your .c file must contain a main function.
+• Write a program that displays its given arguments.
+• Each argument should be printed on a new line,
+in the same order as in the command line.
+• The program should display all arguments except argv[0].
+
+• Example:
+$>./a.out test1 test2 test3 | cat -e
+test1$
+test2$
+test3$
+*/
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i <= (argc - 1))
+	{
+		ft_putstr(argv[i++]);
+		write(1, "\n", 1);
+	}
+}
